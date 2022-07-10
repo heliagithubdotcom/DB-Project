951d031ad0553ce5dd49db92b1c4070213e6fb92
helia <heliyash4380>
2022-07-10 18:50:04 +0430

    urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('store/', views.StoreView.as_view()),
     path('store/<str:store_name>/', views.StoreProductsView.as_view()),
     path('product/<str:product_name>/', views.ProductView.as_view()),
     path('product/<str:product_name>/review/', views.ProductReviewView.as_view()),
     path('product/', views.ProductSortView.as_view()),
+    path('category/', views.CategoryView.as_view()),
+    path('category/<str:category_name>/', views.CategoryProductView.as_view()),
 ]
