4aa41c6e8ede38603a4c68da9e95261dd94a78ab
Nirvana <nirvana.niroomand@gmail.com>
2022-07-14 23:52:41 +0430

    get method changed

--- order/views.py
+++ order/views.py
@@ -2,18 +2,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializers import OrderSerializer, OrderItemSerializer
-from .models import Order, OrderItem
+from .serializers import OrderSerializer, OrderItemSerializer, OrderROSerializer, OrderStatusSerializer
+from .models import Order, OrderItem, OrderStatus
 from shop.models import StoreProduct
 from user.models import UserAddress
 
 
 # Create your views here.
-
-# SELECT * FROM user_useraddress WHERE id == %s
-# SELECT * FROM shop_storeproduct WHERE id == %s
-
 class OrderView(APIView):
+
     def post(self, request):
         data = request.data
         customer_address_id = data.get('customer_address')
@@ -33,11 +30,13 @@ class OrderView(APIView):
             return Response({"message": "customer id is not valid"}, status=status.HTTP_400_BAD_REQUEST)
 
         order = Order.objects.create(customer_address=customer_address, delivery_method=delivery_method)
+        order_status = OrderStatus.objects.create(order=order)
 
         order_items_res = []
         for oi in order_items:
             try:
                 osp = oi['store_product']
+                oic = oi['count']
                 store_product = StoreProduct.objects.raw('SELECT * FROM shop_storeproduct WHERE id == %s', [osp])[0]
             except (IndexError, KeyError):
                 return Response({"message": "store product does not exist"}, status=status.HTTP_400_BAD_REQUEST)
@@ -45,11 +44,40 @@ class OrderView(APIView):
             if store_product.available is False:
                 return Response({"message": "store product is not available"}, status=status.HTTP_400_BAD_REQUEST)
 
-            order_item_obj = OrderItem.objects.create(order=order, store_product=store_product)
+            if oic is None or oic <= 0:
+                return Response({"message": "count not valid."}, status=status.HTTP_400_BAD_REQUEST)
+ 
+            order_item_obj = OrderItem.objects.create(order=order, store_product=store_product, count=oic)
             oi_ser = OrderItemSerializer(order_item_obj)
             order_items_res.append(oi_ser.data)
 
-        ser = OrderSerializer(order)
-        res = ser.data.copy()
+        order_ser = OrderSerializer(order)
+        order_status_ser = OrderStatusSerializer(order_status)
+
+        res = order_ser.data.copy()
+        res.update({"order_status": order_status_ser.data})
         res.update({"order_items": order_items_res})
         return Response(res, status=status.HTTP_201_CREATED)
+
+    def get(self, request):
+        user_id = request.query_params.get('user_id')
+
+        if user_id is None:
+            orders = Order.objects.raw('SELECT * FROM order_order')
+        else:
+            orders = Order.objects.raw('SELECT * FROM order_order INNER JOIN user_useraddress ON order_order.customer_address_id=user_useraddress.id WHERE user_useraddress.user_id == %s', [user_id])
+
+        ser = OrderROSerializer(orders, many=True)
+        response = ser.data.copy()
+
+        for index, order in enumerate(orders):
+            order_items = OrderItem.objects.raw('SELECT * FROM order_orderitem WHERE order_id == %s', [order.id])
+            order_item_ser = OrderItemSerializer(order_items, many=True)
+
+            order_status = OrderStatus.objects.raw('SELECT * FROM order_orderstatus WHERE order_id == %s ORDER BY created_at DESC', [order.id])[0]
+            order_status_ser = OrderStatusSerializer(order_status)
+
+            response[index]['order_items'] = order_item_ser.data
+            response[index]['order_status'] = order_status_ser.data
+
+        return Response(response, status=status.HTTP_200_OK)
