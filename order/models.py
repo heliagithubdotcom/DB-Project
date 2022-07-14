b4c77155b40e9f1b1b0a2fefd8266712a78be20c
Nirvana <nirvana.niroomand@gmail.com>
2022-07-14 23:50:35 +0430

    order status model and serializer created

--- order/models.py
+++ order/models.py
@@ -2,22 +2,19 @@ from django.db import models
 from shop.models import StoreProduct
 from user.models import UserAddress
 
-#CREATE TABLE ORDER
-#(
- # OrderID INT NOT NULL,
-  #Orderdate DATE NOT NULL,
-  #Method VARCHAR(20) NOT NULL,
-  #UserID INT NOT NULL,
-  #AddressID INT NOT NULL,
-  #CodeID INT NOT NULL,
-  #PRIMARY KEY (OrderID),
-  #FOREIGN KEY (UserID) REFERENCES USER(UserID),
-  #FOREIGN KEY (AddressID) REFERENCES ADDRESSES(AddressID),
-  #FOREIGN KEY (CodeID) REFERENCES PROMOTION(CodeID),
-#);
-# Create your models here.
 
-
+# CREATE TABLE Order
+# (
+#  OrderID INT NOT NULL,
+#   created_at DATE NOT NULL,
+#   delivery_method VARCHAR(2) NOT NULL,
+#   AddressID INT NOT NULL,
+#   CodeID INT NOT NULL,
+#   PRIMARY KEY (OrderID),
+#   FOREIGN KEY (UserID) REFERENCES USER(UserID),
+#   FOREIGN KEY (AddressID) REFERENCES ADDRESSES(AddressID),
+#   FOREIGN KEY (CodeID) REFERENCES PROMOTION(CodeID),
+# );
 class Order(models.Model):
     POST = 'PT'
     SNAPP = 'SP'
@@ -33,19 +30,37 @@ class Order(models.Model):
     delivery_method = models.CharField(max_length=2, choices=DELIVERY_CHOICES, default=POST)
     created_at = models.DateTimeField(auto_now_add=True)
 
-#CREATE TABLE PROORDER
-#(
+
+# CREATE TABLE OrderItem
+# (
 #  ProductID INT NOT NULL,
 #  OrderID INT NOT NULL,
 #  TotalPrice INT NOT NULL,
 #  FOREIGN KEY ProductID REFERENCES PRODUCT(ProductID),
 #  FOREIGN KEY OrderID REFERENCES ORDER(OrderID),
 #  PRIMARY KEY ProductID, OrderID,
-#); 
-
-
+# );
 class OrderItem(models.Model):
     id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     store_product = models.ForeignKey(StoreProduct, on_delete=models.CASCADE)
     count = models.PositiveIntegerField()
+
+
+class OrderStatus(models.Model):
+    PAID = 'Paid'
+    DELIVERED = 'Delivered'
+    RECEIVED = 'Received'
+    CANCELED = 'Canceled'
+
+    ORDER_STATUS_CHOICES = (
+        (PAID, 'Paid'),
+        (DELIVERED, 'Delivered'),
+        (RECEIVED, 'Received'),
+        (CANCELED, 'Canceled'),
+    )
+
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    status = models.CharField(max_length=15, choices=ORDER_STATUS_CHOICES, default=PAID)
+    created_at = models.DateTimeField(auto_now_add=True)
--- order/serializers.py
+++ order/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import Order, OrderItem
+from .models import Order, OrderItem, OrderStatus
 
 
 class OrderItemSerializer(ModelSerializer):
@@ -14,3 +14,23 @@ class OrderSerializer(ModelSerializer):
         model = Order
         fields = '__all__'
         depth = 1
+
+
+class OrderItemROSerializer(ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+        depth = 2
+
+
+class OrderROSerializer(ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+        depth = 3
+
+
+class OrderStatusSerializer(ModelSerializer):
+    class Meta:
+        model = OrderStatus
+        fields = '__all__'
